5329c91d6038364f30e25ec98ef59fab8f3709e7
kor-seonwoo <tjsdn_9624@naver.com>
2023-09-20 16:36:05 +0900

    firebase set

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpxlUt6I19pzUXAZ6Zk7It2BnXCNs_xHI",
+  authDomain: "nwitter2023-d4e9f.firebaseapp.com",
+  projectId: "nwitter2023-d4e9f",
+  storageBucket: "nwitter2023-d4e9f.appspot.com",
+  messagingSenderId: "166931569591",
+  appId: "1:166931569591:web:36e63ef9dd587698b32632"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
