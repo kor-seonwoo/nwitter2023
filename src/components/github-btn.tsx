a14835736353eaca9fec346dac9d509809028523
kor-seonwoo <tjsdn_9624@naver.com>
2023-11-17 11:06:30 +0900

    title 변경 및 깃헙 로그인 시 db에 유저필수정보 저장

--- src/components/github-btn.tsx
+++ src/components/github-btn.tsx
@@ -1,7 +1,8 @@
 import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
 import styled from "styled-components";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import { doc, setDoc } from "firebase/firestore";
 
 const Button = styled.span`
     display: flex;
@@ -23,13 +24,34 @@ const Logo = styled.img`
 
 export default function GithubButton() {
     const navigate = useNavigate();
+    // const onClick = async () => {
+    //     try{
+    //         const provieder = new GithubAuthProvider();
+    //         await signInWithPopup(auth, provieder);
+    //         navigate("/");
+    //     } catch (erroer){
+    //         console.log(erroer);
+    //     }
+    // }
     const onClick = async () => {
         try{
-            const provieder = new GithubAuthProvider();
-            await signInWithPopup(auth, provieder);
-            navigate("/");
-        } catch (erroer){
-            console.log(erroer);
+            const provider = new GithubAuthProvider();
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user;
+
+            if (user) {
+                const docRef = doc(db,'userList',user.uid);
+                await setDoc(docRef,{
+                    name: user.displayName || '',
+                    email: user.email || '',
+                    hasProfileImage: user.photoURL ? user.photoURL : false,
+                });
+                navigate("/");
+            } else {
+                console.log('User information is not available.');
+            }
+        } catch (error){
+            console.log(error);
         }
     }
     return (
